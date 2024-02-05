55a4511dbe26a61d793f16a7efccb02f3feb2d6a
oturikumwe <oturikumwe@gmail.com>
2024-02-05 15:02:01 -0800

    9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += (a ** b) / i
+        except Exception as e:
+            result = b + a
+            print(f"Exception occurred: {e}")
+            break
+    return result
