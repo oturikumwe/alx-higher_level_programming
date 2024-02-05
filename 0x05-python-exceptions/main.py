e9c1f5556a5f8184aff5f325fccc014b86482146
oturikumwe <oturikumwe@gmail.com>
2024-02-05 12:20:50 -0800

    0. Safe list printing

---
+++ 0x05-python-exceptions/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+
