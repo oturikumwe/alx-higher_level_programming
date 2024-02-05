9a4b4d9268a56392489efbd66a37ad32bc4decdc
oturikumwe <oturikumwe@gmail.com>
2024-02-05 12:22:57 -0800

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    printed_elements = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
-            num += 1
+            print(my_list[i], end=" ")
+            printed_elements += 1
         except IndexError:
             break
-    print("")
-    Return
+    print()  # Add a newline after printing all elements
+    return printed_elements
