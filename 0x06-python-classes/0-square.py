26dc34321b6170927f77a0151372901ae309556b
oturikumwe <oturikumwe@gmail.com>
2024-02-06 15:06:53 -0800

    0. My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+
+    """Represents a square."""
+
+    def __init__(self):
+
+        """Initializes a new Square instance."""
+        pass
